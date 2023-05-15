d4fe96134af343c25c525802aff31894c84a96ec
Kohara <1k8aioon@gmail.com>
2023-05-15 17:33:41 +0900

    create guessing_game.

---
+++ firs_project/guessing_game/target/debug/deps/guessing_game-0debe49f65907dd1.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/guessing_game/target/debug/deps/guessing_game-0debe49f65907dd1: src/main.rs
+
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/guessing_game/target/debug/deps/guessing_game-0debe49f65907dd1.d: src/main.rs
+
+src/main.rs:
---
+++ firs_project/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/guessing_game/target/debug/guessing_game: /mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/guessing_game/src/main.rs
