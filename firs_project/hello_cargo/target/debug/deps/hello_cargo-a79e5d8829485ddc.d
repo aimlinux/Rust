2d65eb4bdb582f3527ccd4749a3e88516596a0d3
Kohara <1k8aioon@gmail.com>
2023-05-15 16:59:16 +0900

    create hello_cargo and  cargo new project.

---
+++ firs_project/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ firs_project/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/src/main.rs
