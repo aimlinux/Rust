b27d4307b495777f79ebb3921cb34dfe248aa486
Kohara <1k8aioon@gmail.com>
2023-05-15 17:10:57 +0900

    Create target/release.

---
+++ firs_project/hello_cargo/target/release/deps/hello_cargo-bf214a0eaa9ea862.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/target/release/deps/hello_cargo-bf214a0eaa9ea862: src/main.rs
+
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/target/release/deps/hello_cargo-bf214a0eaa9ea862.d: src/main.rs
+
+src/main.rs:
---
+++ firs_project/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/target/release/hello_cargo: /mnt/c/Users/kxiyt/Documents/GitHub/Rust/firs_project/hello_cargo/src/main.rs
