b05ea0d66a7b4bdc14e4e9531805e255965f5b2c
Kohara <1k8aioon@gmail.com>
2023-05-19 15:48:38 +0900

    We wanted to ToDo_App. But, I gave up.

---
+++ ToDo_App/todo-app/target/debug/deps/todo_app-0be1a836ecf6439a.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/ToDo_App/todo-app/target/debug/deps/todo_app-0be1a836ecf6439a: src/main.rs
+
+/mnt/c/Users/kxiyt/Documents/GitHub/Rust/ToDo_App/todo-app/target/debug/deps/todo_app-0be1a836ecf6439a.d: src/main.rs
+
+src/main.rs:
